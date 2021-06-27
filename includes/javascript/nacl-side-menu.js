2e5c8a9b79acf54f5ecc760e4d2f29760a79a4f0
Nino Arbasetti <micrositedesign@gmail.com>
2021-06-27 16:23:18 -0700

    Initial Upload

---
+++ includes/javascript/nacl-side-menu.js
@@ -0,0 +1,9 @@
+function openNav2() {
+  document.getElementById("mySidebar").style.width = "350px";
+  document.getElementById("main").style.marginLeft = "350px";
+}
+
+function closeNav2() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
